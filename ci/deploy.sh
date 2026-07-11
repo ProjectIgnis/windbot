9c8e871585f08451fee79879e672b26b19ffb385
Edoardo Lolletti <edoardo762@gmail.com>
2026-07-11 23:06:07 +0200

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,6 +9,6 @@ git init
 git checkout --orphan $DEPLOY_BRANCH
 git config user.email github-actions[bot]@users.noreply.github.com
 git config user.name "github-actions[bot]"
-git add -A WindBot
+git add -A
 git commit -qm "Deploy $DEPLOY_REPO to $DEPLOY_REPO:$DEPLOY_BRANCH"
 git push -qf https://$DEPLOY_TOKEN@github.com/$DEPLOY_REPO.git $DEPLOY_BRANCH:$DEPLOY_BRANCH
