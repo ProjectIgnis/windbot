348bbaf330163124425332d97a2d8c1238d7e926
Edoardo Lolletti <edoardo762@gmail.com>
2026-07-11 20:29:06 +0200

    update

--- ci/install-patched-mono-runtime.sh
+++ ci/install-patched-mono-runtime.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-rm "~/.nuget/packages/embeddinator-4000/0.4.0/external/Xamarin.Android/lib/xbuild/Xamarin/Android/lib/arm64-v8a/libmonosgen-2.0.so"
+rm "$NuGetPackageRoot/embeddinator-4000/0.4.0/external/Xamarin.Android/lib/xbuild/Xamarin/Android/lib/arm64-v8a/libmonosgen-2.0.so"
 
-cp ./ci/libmonosgen-2.0-assert-patch.so "~/.nuget/packages/embeddinator-4000/0.4.0/external/Xamarin.Android/lib/xbuild/Xamarin/Android/lib/arm64-v8a/libmonosgen-2.0.so"
+cp ./ci/libmonosgen-2.0-assert-patch.so "$NuGetPackageRoot/embeddinator-4000/0.4.0/external/Xamarin.Android/lib/xbuild/Xamarin/Android/lib/arm64-v8a/libmonosgen-2.0.so"
