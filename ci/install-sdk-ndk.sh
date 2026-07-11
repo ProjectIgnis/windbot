c8a0e9ab0a8ff2fdfeca6f197ad45945aecdd83c
Edoardo Lolletti <edoardo762@gmail.com>
2026-07-11 23:23:56 +0200

    reduce steps

--- ci/install-sdk-ndk.sh
+++ ci/install-sdk-ndk.sh
@@ -2,20 +2,13 @@
 
 set -euxo pipefail
 
-# Embeddinator uses Xamarin Android Tools to find Android SDKs, NDKs, and Java SDKs
-# The main registry key is decided by the envvar read here under Visual Studio and MSBuild
-# https://github.com/xamarin/xamarin-android-tools/blob/294f4471a76da6df798c8520a8f8da0e6e83d3a5/src/Xamarin.Android.Tools.AndroidSdk/Sdks/AndroidSdkWindows.cs#L65
-# that is set by Xamarin Android here in the build tasks
-# https://github.com/xamarin/xamarin-android/blob/d4c8f077faefdfc6174355848a9d8c74ecaa8f56/src/Xamarin.Android.Build.Tasks/Tasks/SetVsMonoAndroidRegistryKey.cs
-# and ultimately obtained by reading out the Visual Studio installation id, which seems to be unique
-# https://github.com/xamarin/xamarin-android/blob/a677c1794db64d5559f53a960927447bac3063a2/src/Xamarin.Android.Build.Tasks/MSBuild/Xamarin/Xamarin.Android.Sdk.props#L16
-
 # Manually install Android NDK r15c, the most recent version that still works with Embeddinator 0.4.0
 curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://dl.google.com/android/repository/android-ndk-r15c-linux-x86_64.zip
 echo "0bf02d4e8b85fd770fd7b9b2cdec57f9441f27a2 *android-ndk-r15c-linux-x86_64.zip" | sha1sum -c
 mkdir -p "$ANDROID_SDK_ROOT/ndk"
 unzip android-ndk-r15c-linux-x86_64.zip -d "$ANDROID_SDK_ROOT/ndk"
 
+# Embeddinator loads the paths to find to find Android SDKs, NDKs, and Java SDKs from ~/.config/xbuild/monodroid-config.xml
 mkdir -p ~/.config/xbuild
 cat <<EOF > ~/.config/xbuild/monodroid-config.xml
 <?xml version="1.0" encoding="utf-8"?>
@@ -27,6 +20,6 @@ cat <<EOF > ~/.config/xbuild/monodroid-config.xml
 EOF
 
 # Manually install Android SDK Platform 24, the most recent version that still works with Embeddinator 0.4.0
-# Manually install Android SDK Platform 25, version needed by Xamarin
+# Manually install Android SDK Platform 25, build tools 30.0.3 and cmdline-tools 5.0, required by Xamarin
 (yes || true) | ${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager --sdk_root=$ANDROID_SDK_ROOT "platform-tools" "build-tools;30.0.3" "platforms;android-24" "platforms;android-25" "cmdline-tools;5.0"
 
